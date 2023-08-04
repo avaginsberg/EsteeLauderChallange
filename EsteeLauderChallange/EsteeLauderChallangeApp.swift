450af4fa1c6ee488e6dc0f6c5ccb6ee32be8f0d4
Ava <avaginsberg08@gmail.com>
2023-08-04 19:01:56 -0400

    Initial Commit

---
+++ EsteeLauderChallange/EsteeLauderChallangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EsteeLauderChallangeApp.swift
+//  EsteeLauderChallange
+//
+//  Created by Nathan Ginsberg on 8/4/23.
+//
+
+import SwiftUI
+
+@main
+struct EsteeLauderChallangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
